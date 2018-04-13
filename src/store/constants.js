6ea3729e9ff43fa1fef009910e7b325dd8982118
Kostiantyn Koval <kostiantyn.koval@gmail.com>
2018-04-13 16:34:59 +0300

    BASE_URL changed

--- src/store/constants.js
+++ src/store/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://207.154.215.144:8080/api';
+export const BASE_URL = '/api';
 
 export const GET_DATA_REQUEST = 'GET_DATA_REQUEST';
 export const GET_DATA_SUCCESS = 'GET_DATA_SUCCESS';
